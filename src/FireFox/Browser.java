fac5f3f05f84f2a29db539659241d93ada969282
Haseena-ASH <mohammadhaseena78@gmail.com>
2025-03-04 15:50:25 +0530

    Update Browser.java
    browser commit

--- src/FireFox/Browser.java
+++ src/FireFox/Browser.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+//browser
 public class Browser {
 	public static void main(String[]args) {
 WebDriver driver=new FirefoxDriver();
